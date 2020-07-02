3f96f9ec062f05e859249af6407e6f280aaa66d4
Emmanuel Adebayo <eadebayo15@gmail.com>
2020-07-02 16:15:50 +0100

    Modified

--- routes/view.js
+++ routes/view.js
@@ -5,32 +5,32 @@ const viewController = require('../controllers/viewsController');
 
 const router = express.Router();
 
-router.get('/', auth, viewController.index);
+router.get('/', authController.protect, viewController.index);
 
-router.get('/albums', auth, viewController.albumOverview);
+router.get('/albums', authController.protect, viewController.albumOverview);
 
-router.get('/albums/add', auth, viewController.addAlbum);
+router.get('/albums/add', authController.protect, viewController.addAlbum);
 
-router.post('/albums/add', auth, viewController.uploadAlbumCover, viewController.createAlbum);
+router.post('/albums/add', authController.protect, viewController.uploadAlbumCover, viewController.createAlbum);
 
-router.get('/albums/details/:id', auth, viewController.albumDetailPage);
+router.get('/albums/details/:id', authController.protect, viewController.albumDetailPage);
 
-router.get('/edit/:id', auth, viewController.editAlbumPage);
+router.get('/edit/:id', authController.protect, viewController.editAlbumPage);
 
-router.put('/edit/:id', auth, viewController.uploadAlbumCover, viewController.updateAlbum);
+router.put('/edit/:id', authController.protect, viewController.uploadAlbumCover, viewController.updateAlbum);
 
-router.delete('/delete/:id', auth, viewController.deleteAlbum);
+router.delete('/delete/:id', authController.protect, viewController.deleteAlbum);
 
-router.get('/genres', auth, viewController.getAllGenres);
+router.get('/genres', authController.protect, viewController.getAllGenres);
 
-router.get('/genres/add', auth, viewController.addGenreForm);
+router.get('/genres/add', authController.protect, viewController.addGenreForm);
 
-router.post('/genres/add', auth, viewController.createGenre);
+router.post('/genres/add', authController.protect, viewController.createGenre);
 
-router.get('/genres/edit/:id', auth, viewController.editGenrePage);
+router.get('/genres/edit/:id', authController.protect, viewController.editGenrePage);
 
-router.put('/genres/edit/:id', auth, viewController.updateGenre);
+router.put('/genres/edit/:id', authController.protect, viewController.updateGenre);
 
-router.delete('/genres/delete/:id', auth, viewController.deleteGenre);
+router.delete('/genres/delete/:id', authController.protect, viewController.deleteGenre);
 
 module.exports = router;

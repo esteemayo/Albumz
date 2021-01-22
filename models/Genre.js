07a4b6b6e45f8d32c59a88b222917ebf695226c9
Emmanuel Adebayo <eadebayo15@gmail.com>
2021-01-22 22:45:58 +0100

    Adds slug to the model

--- models/Genre.js
+++ models/Genre.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const genreSchema = new mongoose.Schema({
     name: {
@@ -6,15 +7,33 @@ const genreSchema = new mongoose.Schema({
         required: [true, 'A genre must have a name'],
         unique: true
     },
+    slug: String,
     user: {
-        type: String,
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
         required: [true, 'There must be a logged in user']
     },
     createdAt: {
         type: Date,
-        default: Date.now(),
-        select: false
+        default: Date.now()
     }
+});
+
+genreSchema.pre('save', function(next) {
+    if (!this.isModified('name')) return next();
+
+    this.slug = slugify(this.name, { lower: true });
+
+    next();
+});
+
+genreSchema.pre(/^find/, function(next) {
+    this.populate({
+        path: 'user',
+        select: 'firstName lastName email photo'
+    });
+
+    next();
 });
 
 const Genre = mongoose.model('Genre', genreSchema);

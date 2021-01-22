0173c3941e3cd5e7a46561b3ccff980ab6c42a1e
Emmanuel Adebayo <eadebayo15@gmail.com>
2021-01-22 22:27:57 +0100

    Connect the application to mongo atlas

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,19 @@
+/* eslint-disable */
 const mongoose = require('mongoose');
 const config = require('config');
 
+// MongoDB Atlas
+const db = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+// Db local
+const dbLocal = config.get('db');
+
 module.exports = () => {
-    mongoose.connect(config.get('db'), {
+    mongoose.connect(dbLocal, {
+    // mongoose.connect(db, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useUnifiedTopology: true,
@@ -10,7 +21,7 @@ module.exports = () => {
     })
         .then(con => {
             // console.log(con.connections);
-            console.log('MongoDB Connected...')
-        })
-        .catch(err => console.log(`COULD NOT CONNECT TO MONGODB: ${err}`));
+            // console.log(`MongoDB Connected → ${db}`)
+            console.log(`MongoDB Connected → ${dbLocal}`)
+        });
 }
